3e5e608e216b34922b6192e6e9a0436109d1040b
Selina Ochsner <selina@selinas-mbp.localdomain>
2020-12-02 16:37:36 +0100

    msg2

--- src/shop/static_products.js
+++ src/shop/static_products.js
@@ -71,17 +71,9 @@ const _products = [
     imageUrl: '/img/Shop7Weinhaenger1.jpg',
     inventory: 1,
   },
+  
   {
     id: 8,
-    name: 'Richtung Wolke 7 Hänger',
-    description:
-      'Die Richtung Wolke 7 Hänger lassen Raum für Fantasie. Sie hängen direkt an einer Öse. 925er Silber.',
-    price: 125,
-    imageUrl: '/img/Shop8Richtungwolke7haenger.jpg',
-    inventory: 1,
-  },
-  {
-    id: 9,
     name: 'Natur Hänger',
     description:
       'Die Natur Hänger komplementieren jeden Tag wie den anderen. Sie hängen für sich selbst. Mit einer Variation aus Achat-, Opal- und Türkisperlen. 925er Silber.',
@@ -90,7 +82,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 10,
+    id: 9,
     name: 'Natur Hoops',
     description:
       'Die Naturhoops sind klassisch neu arrangiert, ein echter Alltimer. Sie messen ca. 17 cm diagonal. 925er Silber.',
@@ -99,7 +91,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 11,
+    id: 10,
     name: 'Natur Kette',
     description:
       'Die Natur Kette verfeinert dein Dekoltee mit schlichter Eleganz. Sie besteht aus den länglichen handgelöteten Ösen und einer Variation aus Achat-, Opal- und Türkisperlen. Schließen lässt sie sich durch einen S-Haken. 925er Silber.',
@@ -108,7 +100,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 12,
+    id: 11,
     name: 'Natur Armband',
     description:
       'Das Natur Armband erscheint nett neben deiner Hand. Es besteht aus den länglichen Ösen und einer Variation aus Achat-, Opal- und Türkisperlen. Schließen lässt es sich durch einen S-Haken. 925er Silber.',
@@ -117,7 +109,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 13,
+    id: 12,
     name: 'Vereint Armband',
     description:
       'Das Vereint Armband schmeichelt uns allen, wie wir zusammen stehen in all unseren Farben und Formen. Hier hängen vier unserer lieben Anhänger sowie eine Variation aus Achat-, Opal- und Türkisperlen. 925er Silber.',
@@ -126,7 +118,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 14,
+    id: 13,
     name: 'Vereint Kette',
     description:
       'Die Vereint Kette ist die Symbiose aus allem was du erschaffst. Hier hängen fünf der süßen Anhänger und eine Variation aus Achat-, Opal- und Türkisperlen. 925er Silber.',
@@ -135,7 +127,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 15,
+    id: 14,
     name: 'Ringe',
     description:
       'Drei simple Vierkantringe als Set. Ein angenehmer Begleiter in jeder Komposition. 925er Silber.',
@@ -144,7 +136,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 16,
+    id: 15,
     name: '7 Hoops',
     description:
       'Die 7 Hoops sind leicht und erregen Aufmerksamkeit zu gleich. 925er Silber mit einer Variation an Opal-, Achat- und Türkisperlen.',
@@ -153,7 +145,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 17,
+    id: 16,
     name: 'Drei Männchen Hänger',
     description:
       'Die Drei Männchen Hänger sind eine Reihung des Männchens und ergeben ein Ornament, welches jedes Ohr noch interessanter macht. 925er Silber. Vergoldet auf Anfrage.',
@@ -162,7 +154,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 18,
+    id: 17,
     name: 'Drei Männchen Kette',
     description:
       'Die Drei Männchen Kette ziert vertikal das Dekolleté. Auch hier ein wunderbares Ornament an einer leichten Kette. 925er Silber.',
@@ -171,7 +163,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 19,
+    id: 18,
     name: 'Voll Wolke 7 Kette',
     description:
       'Die Voll Wolke 7 Kette ist eine Reihung unseres Wolke 7 Schriftzuges. Durch ihre Form erregt sie zurückhaltende Aufmerksamkeit. 925er Silber..',
@@ -180,7 +172,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 20,
+    id: 19,
     name: 'Kussmund Ring',
     description:
       'Der Kussmund Ring trägt sich nett an deinem Finger. Er wird verziert von einer Variation aus Opal-, Achat- und Türkisperlen. 925er Silber.',
@@ -189,7 +181,7 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 21,
+    id: 20,
     name: 'Simpel Armband vergoldet',
     description:
       'Das vergoldete Simpel Armband ist ein souveräner Hingucker. Klassisch und modern zugleich. Durch den gehämmerten S-Haken in Länge verstellbar. Im Kern Silber 925er, außen Gelbgold plattiert.',
@@ -198,10 +190,10 @@ const _products = [
     inventory: 1,
   },
   {
-    id: 22,
+    id: 21,
     name: 'Wein Hänger vergoldet',
     description:
-      'Die Wein Hänger und nun auch noch vergoldet. Für jeden der grundsätzlich goldfarbend trägt ein Muss. Silber 925er Kern, außen Gelbgold plattiert.',
+      'Die Wein Hänger! ... und nun auch noch vergoldet. Für jeden der grundsätzlich goldfarbend trägt ein Muss. Silber 925er Kern, außen Gelbgold plattiert.',
     price: 225,
     imageUrl: '/img/Shop23vergoldetWeinhaenger.jpg',
     inventory: 1,
